dc56880703f3a711eb541c20ede6edd50cba2077
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-08-21 16:26:46 +0300

    New Sound Effects Added

---
+++ EscapeProtocol/Assets/Scripts/Movements/PlayerSoundManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Movements
+{
+    public class PlayerSoundManager : MonoBehaviour
+    {
+        [SerializeField] private AudioClip footStepSound;
+        [SerializeField] private AudioClip footStepSound2;
+        [SerializeField] private AudioClip backStepSound;
+        [SerializeField] private AudioClip fallSound;
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+        
+        public void BackStepSound()
+        {
+            _audioSource.PlayOneShot(backStepSound);
+        }
+        
+        public void FallSound()
+        {
+            _audioSource.PlayOneShot(backStepSound);
+        }
+    }
+}
