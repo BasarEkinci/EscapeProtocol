b352246f28f4de393008a6bdc84da706749aab6c
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-08-29 10:21:58 +0300

    Heal and Damage Effect
    Heal and damage effect added to player and npc

--- EscapeProtocol/Assets/Scripts/Controllers/Enemy/EnemyHealthController.cs
+++ EscapeProtocol/Assets/Scripts/Controllers/Enemy/EnemyHealthController.cs
@@ -2,6 +2,7 @@
 using Combat;
 using Cysharp.Threading.Tasks;
 using Data.UnityObjects;
+using DG.Tweening;
 using Managers;
 using UnityEngine;
 
@@ -12,18 +13,30 @@ namespace Controllers.Enemy
         [SerializeField] private ParticleSystem explosionParticle;
         [SerializeField] private SoundDataScriptable soundData;
         [SerializeField] private EnemyDataScriptable enemyData;
+        [SerializeField] private Material material;
         public int Health => _currentHealth;
         public bool IsDead => _currentHealth <= 0;
-       
+        
         
         private int _currentHealth;
+        private Color _defaultColor;
+        private Color _damagedColor;
+        
         private void Awake()
         {
             _currentHealth = enemyData.HealthData.Health;
         }
+
+        private void Start()
+        {
+            _defaultColor = material.color;
+            _damagedColor = Color.red;
+        }
+
         public void TakeDamage(int damage)
         {
             _currentHealth -= damage;
+            material.DOColor(_damagedColor, 0.1f).OnComplete(() => material.DOColor(_defaultColor, 0.1f));
             _currentHealth = Mathf.Max(_currentHealth, 0);
             if (_currentHealth <= 0)
             {
--- EscapeProtocol/Assets/Scripts/Controllers/Player/PlayerHealthController.cs
+++ EscapeProtocol/Assets/Scripts/Controllers/Player/PlayerHealthController.cs
@@ -2,6 +2,7 @@ using System;
 using Combat;
 using Cysharp.Threading.Tasks;
 using Data.UnityObjects;
+using DG.Tweening;
 using Managers;
 using UnityEngine;
 
@@ -13,22 +14,38 @@ namespace Controllers.Player
         [SerializeField] private ParticleSystem explosionParticle;
         [SerializeField] private SoundDataScriptable soundData;
         [SerializeField] private PlayerDataScriptable playerData;
+        [SerializeField] private Material material;
         #endregion
         
         #region Public Fields
         public int Health => _currentHealth;
         public bool IsDead => _currentHealth <= 0;
         #endregion
-        
+
+        #region Private Variables
         private int _currentHealth;
+        private Color _defaultColor;
+        private Color _damagedColor;
+        private Color _healColor;
+        #endregion
+        
         private void Awake()
         {
             _currentHealth = playerData.HealthData.Health;
         }
+
+        private void Start()
+        {
+            _defaultColor = material.color;
+            _damagedColor = Color.red;
+            _healColor = Color.green;
+        }
+
         public void TakeDamage(int damage)
         {
             _currentHealth -= damage;
             _currentHealth = Mathf.Max(_currentHealth, 0);
+            material.DOColor(_damagedColor, 0.1f).OnComplete(() => material.DOColor(_defaultColor, 0.1f));
             if (_currentHealth <= 0)
             {
                 Death().Forget();
@@ -38,6 +55,7 @@ namespace Controllers.Player
         public void IncreaseHealth(int health)
         {
             _currentHealth += health;
+            material.DOColor(_healColor, 0.1f).OnComplete(() => material.DOColor(_defaultColor, 0.1f));
             _currentHealth = Mathf.Min(_currentHealth, playerData.HealthData.Health);
         }
         private async UniTaskVoid Death()
