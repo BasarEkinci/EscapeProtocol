f97196bc6a9d1eb8bcc2ec8401b8439109b2f5ff
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-09-02 21:25:36 +0300

    Laser Wall Added

--- EscapeProtocol/Assets/Scripts/Objects/Interactable/LaserWall.cs
+++ EscapeProtocol/Assets/Scripts/Objects/Interactable/LaserWall.cs
@@ -1,19 +1,40 @@
 using Combat;
+using Data.UnityObjects;
+using Managers;
 using UnityEngine;
 
 namespace Objects.Interactable
 {
     public class LaserWall : MonoBehaviour
     {
+        [SerializeField] private SoundDataScriptable soundData;
+        [SerializeField] private GameObject hitParticle;
+        private float _nextDamageTime = 0;
+        private float _damageInterval = 1.5f;
         private void OnCollisionEnter(Collision other)
         {
             IDamageable player = other.gameObject.GetComponent<IDamageable>();
+            player?.TakeDamage(30);
+            HitEvents(other.GetContact(0).point);
+        }
 
-            if (player != null)
+
+        private void OnCollisionStay(Collision other)
+        {
+            IDamageable player = other.gameObject.GetComponent<IDamageable>();
+            if (Time.time > _nextDamageTime)
             {
-                player.TakeDamage(30);
-                //Damage Sound
+                player?.TakeDamage(30);
+                HitEvents(other.GetContact(0).point);
+                _nextDamageTime = Time.time + _damageInterval;
             }
+        }
+
+        private void HitEvents(Vector3 position)
+        {
+            SoundManager.PLaySound(soundData, "LaserWallHit");
+            Debug.Log(position);
+            Instantiate(hitParticle,position + Vector3.up,transform.rotation);
         }
     }
 }
