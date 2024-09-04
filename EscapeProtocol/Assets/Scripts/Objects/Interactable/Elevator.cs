340fd17b6cc70f71b6c52e6f6eb698a72a3a63cf
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-09-04 16:31:13 +0300

    Elevator Added

--- EscapeProtocol/Assets/Scripts/Objects/Interactable/Elevator.cs
+++ EscapeProtocol/Assets/Scripts/Objects/Interactable/Elevator.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using DG.Tweening;
 using UnityEngine;
 
-public class Elevator : MonoBehaviour
+namespace Objects.Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Elevator : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Transform floor1;
+        [SerializeField] private Transform floor2;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private int _currentFloor;
         
+        private void Start()
+        {
+          _currentFloor = 1;   
+        }
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.transform.parent = transform;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (_currentFloor == 1)
+                    {
+                        transform.DOMove(floor2.position, 1f).OnComplete(() =>
+                        {
+                            _currentFloor = 2; 
+                        });
+                    }
+                    else if (_currentFloor == 2)
+                    {
+                        transform.DOMove(floor1.position, 1f).OnComplete(() =>
+                        {
+                            _currentFloor = 1; 
+                        });
+                    }
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.transform.parent = null;
+            }
+        }
     }
 }
