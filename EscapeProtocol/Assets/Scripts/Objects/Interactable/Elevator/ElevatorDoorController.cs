d794dbf2929b96de6d9949db7eb85eb8c982bbcc
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-09-09 14:52:04 +0300

    Elevator Animation Edited

--- EscapeProtocol/Assets/Scripts/Objects/Interactable/Elevator/ElevatorDoorController.cs
+++ EscapeProtocol/Assets/Scripts/Objects/Interactable/Elevator/ElevatorDoorController.cs
@@ -1,65 +1,44 @@
-using System;
-using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
 
 namespace Objects.Interactable.Elevator
 {
     public class ElevatorDoorController : MonoBehaviour
     {
-        [SerializeField] private List<Transform> upperDoorsOpenPos;
-        [SerializeField] private List<Transform> lowerDoorsOpenPos;
-        
-        [SerializeField] private List<GameObject> upperDoors;
-        [SerializeField] private List<GameObject> lowerDoors;
-        
-        [SerializeField] private List<Transform> upperDoorsClosedPos;
-        [SerializeField] private List<Transform> lowerDoorsClosedPos;
-        
-        [SerializeField] private float duration;
-        
-        
+        #region Referances
         private ElevatorMovementController _elevatorMovementController;
-
-        private Transform _upperDoor0FirstPos;
-        private Transform _upperDoor1FirstPos;
-        private Transform _lowerDoor0FirstPos;
-        private Transform _lowerDoor1FirstPos;
+        private Animator _animator;
+        #endregion
         
+        #region Private Variables
         private bool _canOpen;
+        private bool _isPlayerNearby;
         private string _playerTag = "Player";
+        #endregion
+
+        #region Animator Hashes
+        private static readonly int CanOpen = Animator.StringToHash("CanOpen");
+        private static readonly int IsPlayerNearby = Animator.StringToHash("IsPlayerNearby");
+        #endregion
         
         private void Awake()
         {
             _elevatorMovementController = GetComponentInParent<ElevatorMovementController>();
-        }
-
-        private void Start()
-        {
-            _upperDoor0FirstPos = upperDoors[0].transform;
-            _upperDoor1FirstPos = upperDoors[1].transform;
-            _lowerDoor0FirstPos = lowerDoors[0].transform;
-            _lowerDoor1FirstPos = lowerDoors[1].transform;
+            _animator = GetComponent<Animator>();
         }
 
         private void Update()
         {
-            if (_elevatorMovementController.IsMoving)
-            {
-                _canOpen = false;
-                CloseDoors();
-            }
-            else
-            {
-                _canOpen = true;
-            }   
+            _canOpen = !_elevatorMovementController.IsMoving;
+            
+            _animator.SetBool(CanOpen, _canOpen);
+            _animator.SetBool(IsPlayerNearby, _isPlayerNearby);
         }
         
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag(_playerTag) && _canOpen)
             {
-               OpenDoors(); 
+                _isPlayerNearby = true;
             }
         }
 
@@ -67,33 +46,10 @@ namespace Objects.Interactable.Elevator
         {
             if (other.CompareTag(_playerTag) && _canOpen)
             {
-                CloseDoors();
+                _isPlayerNearby = false;
             }
         }
-
-        private void OpenDoors()
-        {
-            upperDoors[0].transform.DOMove(upperDoorsOpenPos[0].transform.position, duration).OnComplete(() =>
-            {
-                upperDoors[1].transform.DOMove(upperDoorsOpenPos[1].transform.position, duration);
-            });
-            lowerDoors[0].transform.DOMove(lowerDoorsOpenPos[0].transform.position, duration).OnComplete(() =>
-            {
-                lowerDoors[1].transform.DOMove(lowerDoorsOpenPos[1].transform.position, duration);
-            });
-        }
         
-        private void CloseDoors()
-        {
-            upperDoors[1].transform.DOMove(_upperDoor1FirstPos.position, duration).OnComplete(() =>
-            {
-                upperDoors[0].transform.DOMove(_upperDoor0FirstPos.position, duration);
-            });
-            lowerDoors[1].transform.DOMove(_lowerDoor1FirstPos.position, duration).OnComplete(() =>
-            {
-                lowerDoors[0].transform.DOMove(_lowerDoor0FirstPos.position, duration);
-            });
-        }
     }
     
 }
