50b8818ebbd2d20e996a30485757cee66d0cf6a3
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-09-18 13:29:10 +0300

    Main Menu UI Edited

--- EscapeProtocol/Assets/Scripts/UI/MainMenuUIController.cs
+++ EscapeProtocol/Assets/Scripts/UI/MainMenuUIController.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,19 +7,43 @@ namespace UI
 {
     public class MainMenuUIController : MonoBehaviour
     {
+        [SerializeField] private GameObject panelsParent;
+        [SerializeField] private GameObject mainMenuPanel;
+        [SerializeField] private GameObject creditPanel;
+        [SerializeField] private GameObject optionsPanel;
+
+        [SerializeField] private float duration;
+
+        private float _screenWidth;
+        private float _screenHeight;
+        private void Start()
+        {
+            _screenWidth = Screen.width;
+            _screenHeight = Screen.height;
+        }
+
         public void Play()
         {
-            SceneManager.LoadSceneAsync(sceneBuildIndex: 1);
+            SceneManager.LoadScene(sceneBuildIndex: 1);
+        }
+
+        public void Back()
+        {
+            panelsParent.GetComponent<RectTransform>().DOAnchorPos(Vector2.zero, duration);
+        }
+        
+        public void Credit()
+        {
+            panelsParent.GetComponent<RectTransform>().DOAnchorPos(Vector2.down * _screenHeight, duration);
         }
 
         public void Options()
         {
-            Debug.Log("Options button pressed");
+            panelsParent.GetComponent<RectTransform>().DOAnchorPos(Vector2.left * _screenWidth, duration);
         }
 
         public void Exit()
         {
-            Debug.Log("Exit button pressed");
         }
     }
 }
